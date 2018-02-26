955e66df5548a8ea803180208c4156103a7fcb41
Ruly Rudel <ruly.rudel@gmail.com>
2018-02-26 11:00:18 +0900

    add some files.

--- cc/builtin.c
+++ cc/builtin.c
@@ -1,5 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "builtin.h"
 
+cons_t* _read(FILE* fp)
+{
+	return 0;
+}
 
-int mal_read(void) { return 0; }
 
+cons_t*	cons(const value_t *car, const value_t *cdr)
+{
+	cons_t* c = (cons_t*)malloc(sizeof(cons_t));
+	c->car = *car;
+	c->cdr = *cdr;
+
+	return c;
+}
--- cc/builtin.h
+++ cc/builtin.h
@@ -1,6 +1,32 @@
 #ifndef _builtin_h_
 #define _builtin_h_
 
-int mal_read(void);
+#include <stdint.h>
+
+typedef enum _rtype_t { INT_T = 0, CHAR_T, SYMBOL_T, CONS_T } rtype_t;
+
+struct _cons_t;
+typedef union
+{
+	struct _cons_t*	cons;
+	int		integer;
+	char		character;
+} content_t;
+
+typedef struct
+{
+	rtype_t		type;
+	content_t	content;
+} value_t;
+
+typedef struct _cons_t
+{
+	value_t		car;
+	value_t		cdr;
+} cons_t;
+
+cons_t* _read(FILE* fp);
+cons_t*	cons(const value_t *car, const value_t *cdr);
+
 
 #endif // _builtin_h_
